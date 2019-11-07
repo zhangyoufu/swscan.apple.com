d65cf545df6fb3bea3aa89b35dcf7f29f50ac978
张酉夫 <zhangyoufu@gmail.com>
2019-11-07 11:57:53 +0800

    squash continuous "automatic commit"

--- .github/cron.sh
+++ .github/cron.sh
@@ -10,6 +10,11 @@ if [ -n "$(git status --porcelain)" ]; then
 	git config --global user.email "$(whoami)@$(hostname --fqdn)"
 	git add --all
 	git diff --cached --diff-filter=AM --name-only -z | ./parse.py
-	git commit --all --message 'automatic commit'
-	git push "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:master
+	if [ "$(git log -1 --pretty=format:%B)" = "automatic commit" ]; then
+		git commit --amend --no-edit
+		git push -f "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:master
+	else
+		git commit --message 'automatic commit'
+		git push "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:master
+	fi
 fi
